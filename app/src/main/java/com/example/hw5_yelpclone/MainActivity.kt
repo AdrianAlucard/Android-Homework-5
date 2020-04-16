307175bead0a4b2a6744b9a59dd35e28568b2d6f
AdrianAlucard <wartinator@gmail.com>
2020-04-15 21:21:11 -0400

    added dialogs, google map implicit intent, small changes

--- app/src/main/java/com/example/hw5_yelpclone/MainActivity.kt
+++ app/src/main/java/com/example/hw5_yelpclone/MainActivity.kt
@@ -2,66 +2,103 @@ package com.example.hw5_yelpclone
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import okhttp3.*
 import retrofit2.Call
 import retrofit2.Retrofit
-import java.io.IOException
 import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
     private val BASE_URL = "https://api.yelp.com/v3/"
+    private val businessList: ArrayList<YelpReview> = ArrayList()
+    private lateinit var yelpApi: YelpService
+
     private val token =
-        "<insert API token here>"
+        "<insert api key here>"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val businessList = ArrayList<YelpReview>()
         val adapter = YelpRecyclerAdapter(businessList)
+        yelp_recycler_view.adapter = adapter
+        yelp_recycler_view.layoutManager = LinearLayoutManager(this)
 
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-        val yelpApi = retrofit.create(YelpService::class.java)
+        yelpApi = retrofit.create(YelpService::class.java)
+    }
 
-        yelp_recycler_view.adapter = adapter
+    fun searchButton(view: View) {
+        val foodItem = food_search.text.toString()
+        val locationSearch = location_search.text.toString()
 
-        yelp_recycler_view.layoutManager = LinearLayoutManager(this)
-
-        val results = yelpApi.SearchYelp("pizza", "New Britain", "Bearer $token")
-            .enqueue(object : retrofit2.Callback<YelpResults> {
-                override fun onFailure(call: Call<YelpResults>, t: Throwable) {
-                    Log.d(TAG, "onFailure : $t")
-                }
-
-                override fun onResponse(
-                    call: Call<YelpResults>,
-                    response: retrofit2.Response<YelpResults>
-                ) {
-                    val body = response.body()
-
-                    if (body == null) {
-                        Log.w(TAG, "Valid response was not received")
-                        return
+        if(isInputValid(foodItem, locationSearch)) {
+            yelpApi.SearchYelp(foodItem, locationSearch, "Bearer $token")
+                .enqueue(object : retrofit2.Callback<YelpResults> {
+                    override fun onFailure(call: Call<YelpResults>, t: Throwable) {
+                        Log.d(TAG, "onFailure : $t")
                     }
-                    Log.d(TAG, ": ${body.businesses?.get(0).name}")
-                    Log.d(TAG, ": ${body.businesses?.get(0).image_url}")
-                    Log.d(TAG, ": ${body.businesses?.get(0).review_count}")
-                    Log.d(TAG, ": ${body.businesses?.get(0).rating}")
-                    Log.d(TAG, ": ${body.businesses?.get(0).location.display_address}")
 
-                    businessList.addAll(body.businesses);
-                    adapter.notifyDataSetChanged()
-                }
+                    override fun onResponse(
+                        call: Call<YelpResults>,
+                        response: retrofit2.Response<YelpResults>
+                    ) {
+                        val body = response.body()
+                        Log.d(TAG, body.toString())
 
-            })
+                        if (body == null || body.businesses.isNullOrEmpty()) {
+                            Log.w(TAG, "Valid response was not received")
+                            Toast.makeText(this@MainActivity, "No Results Found", Toast.LENGTH_SHORT).show()
+                            return
+                        }
+                        Log.d(TAG, ": ${body.businesses?.get(0).name}")
+                        Log.d(TAG, ": ${body.businesses?.get(0).image_url}")
+                        Log.d(TAG, ": ${body.businesses?.get(0).review_count}")
+                        Log.d(TAG, ": ${body.businesses?.get(0).rating}")
+                        Log.d(TAG, ": ${body.businesses?.get(0).location.display_address}")
+                        businessList.clear() // remove old search results
+                        businessList.addAll(body.businesses);
+                        (yelp_recycler_view.adapter as YelpRecyclerAdapter).notifyDataSetChanged()
+                    }
+
+                })
+        }
+    }
+
+    /**
+     * when input invalid, show popup modal to inform user of bad input
+     */
+    private fun isInputValid(foodItem: String, locationSearch: String): Boolean {
+        val builder = AlertDialog.Builder(this)
+        var title = "Missing Search Term"
+        var message: String
+
+        if(foodItem.isNullOrBlank()) {
+            message = "Please enter search term in food input field"
+        } else if(locationSearch.isNullOrBlank()) {
+            message = "Please enter location in location input field"
+        } else {
+            return true
+        }
+
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setIcon(R.drawable.ic_delete)
+        builder.setNeutralButton("Dismiss") {dialog, which ->
+            dialog.dismiss()
+        }
+
+        builder.create().show()
+        return false
     }
 }
--- app/src/main/java/com/example/hw5_yelpclone/YelpRecyclerAdapter.kt
+++ app/src/main/java/com/example/hw5_yelpclone/YelpRecyclerAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.hw5_yelpclone
 
+import android.content.Intent
+import android.net.Uri
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.row_item.view.*
 import java.util.ArrayList
 
@@ -23,11 +26,16 @@ class YelpRecyclerAdapter(private val reviews: ArrayList<YelpReview>) : Recycler
         holder.businessName.text = currentYelpReview.name
         holder.reviewCount.text = currentYelpReview.review_count.toString()
         holder.ratingBar.rating = currentYelpReview.rating.toFloat()
-        holder.distance.text = convertToMiles(currentYelpReview.distance).toString()
+        holder.distance.text = formattedDistance(currentYelpReview.distance)
         holder.address.text = "${currentYelpReview.location.display_address[0]}, ${currentYelpReview.location.display_address[1]}"
         holder.category.text = currentYelpReview.categories[0].title
-        // use Picasso to load image url
-        holder.image.setImageResource(R.drawable.ic_launcher_foreground)
+
+        if(!currentYelpReview.image_url.isNullOrEmpty()) {
+            Picasso.get().load(currentYelpReview.image_url).into(holder.image)
+        } else {
+            holder.image.setImageResource(R.drawable.ic_launcher_foreground)  //image place holder
+        }
+
         holder.price.text  = currentYelpReview.price
     }
 
@@ -35,12 +43,17 @@ class YelpRecyclerAdapter(private val reviews: ArrayList<YelpReview>) : Recycler
         return reviews.size
     }
 
+    private fun formattedDistance(distance: Double): String {
+        val distance = convertToMiles(distance)
+        return "${String.format("%.2f", distance)} mi"
+    }
+
     private fun convertToMiles(distance: Double): Double {
         return distance / 1609.344 // magic number. yay conversions!
     }
 
     // provides reference to the views
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val businessName = itemView.business_name
         val reviewCount = itemView.num_reviews
         val ratingBar = itemView.ratingBar
@@ -49,5 +62,16 @@ class YelpRecyclerAdapter(private val reviews: ArrayList<YelpReview>) : Recycler
         val category = itemView.category
         val image = itemView.business_image
         val price = itemView.price
+
+        init {
+            itemView.setOnClickListener{
+                val selectedItem = reviews[adapterPosition]
+                val address = selectedItem.location.display_address[0] + "," + selectedItem.location.display_address[1]
+
+                val intent = Intent(Intent.ACTION_VIEW,
+                    Uri.parse("google.navigation:q=$address"))
+                itemView.context.startActivity(intent)
+             }
+        }
     }
 }
